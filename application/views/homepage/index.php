e93f716faf5b4a3e5d3af9202d941a25c742d397
bhadresh siddhapara <b4bhadresh2020@gmail.com>
2022-05-19 10:55:25 +0530

    Update index.php

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -806,7 +806,7 @@
             // since particles fall down, start a bit higher than random
             confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
             confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-            }, 250);
+            }, 600);
         }
     // END:: confetti -DAB3
     </script>
